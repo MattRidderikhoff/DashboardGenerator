20d22624c67d4864519a0bc799bc6a27128f2b3a
r2i0b <r2i0b@ugrad.cs.ubc.ca>
2018-09-27 22:19:05 -0700

    completed parsing of .csv files into useable dataset

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -14,8 +14,10 @@ class BaseController extends AbstractController
 {
     public function renderHomepage() {
 
-        // PSEUDOCODE
         // 1. Parse dataset.csv into a structure we can use
+        $dataset_path = str_replace('Controller', '', __DIR__ ). 'Input/movies_2011.csv';
+        $dataset = $this->get('serializer')->decode(file_get_contents($dataset_path), 'csv');
+
         // 2. Tokenize Input.txt and generate an AST
         // 3. Evaluate AST and generate the javascript + html for each individual chart
         // 4. Group chart html if applicable
